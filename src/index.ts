9ccac1474585abcbbf0d45cb1a9e06eb1b39810c
rozzzly <pclienau@gmail.com>
2017-02-26 16:01:44 -0600

    remove prepExports

--- src/index.ts
+++ src/index.ts
@@ -13,44 +13,32 @@ module.exports.pitch = function(this: webpack.loader.LoaderContext, remainingReq
     if (this.cacheable) this.cacheable();
 
     const remainingRequestRequirePath: string = stringifyRequest(this, `!!${remainingRequest}`);
-    const prepExportsRequirePath: string = stringifyRequest(this, `!${path.join(__dirname, './prepExports')}`);
+    const registryRequirePath: string = stringifyRequest(this, `!${path.join(__dirname, './registry')}`);
     return `
-<<<<<<< HEAD
-        var prepExports = require(${prepExportsRequirePath}).default;
-        var args = {
-            exports: module.exports,
-            inline: ${!!cfgOptions.inline},
-            moduleID: module.id,
-            remainingRequest: '${remainingRequestRequirePath}'
-        };
-=======
         var content = undefined;
         var register = require(${registryRequirePath}).register;
 
         function load() {
             content = require(${remainingRequestRequirePath});
-
             if (typeof content === 'string') {
                 content = [[module.id, content, '']];
             }
-
+            var css = '';
+            for(var i = 0; i < content.length; i++) {
+                css += content[i][1];
+            }
             module.exports = content.locals || {};
             module.exports.${ImportID} = {
                 moduleID: content[0][0],
                 css: ${cfgOptions.inline ? 'content[0][1]' : 'false'}
             }
-
-            register(content[0][0], content[0][1]);
+            register(content[0][0], css);
         }
->>>>>>> parent of a607be0... add support for multiple exports added by css-loader
-
         if (module.hot && typeof window !== 'undefined' && !!window.document) {
-            module.hot.accept(${remainingRequestRequirePath}, function() {
-                prepExports(args);
-            });
+            module.hot.accept(${remainingRequestRequirePath}, load);
         }
 
-        prepExports(args);
+        load();
     `;
 };
 
